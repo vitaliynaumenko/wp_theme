c3e29fd5f0bdf34c26549aefbd1386ed07c96fa5
vn <bambiino89@gmail.com>
2020-08-31 19:43:09 +0300

    added new files and add laravel-mix

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,7 @@ import jQuery from "jquery";
 
 window.$ = window.jQuery = jQuery;
 
-window.addEventListener('DOMContentLoaded', function () {
 
-    let swiperSliderModule = document.querySelectorAll('.swiper-slider')
-    if (swiperSliderModule.length) {
-
-
-
-        import(
-            /* webpackChunkName: "swiperSliderModule" */
-            /* webpackPrefetch: true */
-
-            '../js/components/swiper'
-            )
-            .then((module) => {
-                module.default(swiperSliderModule)
-            })
-    }
-//
-})
 
 
 
